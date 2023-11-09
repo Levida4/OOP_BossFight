369004b2a3cdaf796badbe5f89fcd3af2402f985
Levi <mogli685@googlemail.com>
2023-11-09 14:33:29 +0100

    create functions

--- BossKampf.cpp
+++ BossKampf.cpp
@@ -8,4 +8,27 @@ class Charakter {
 protected:
 	string name;
 	int leben;
-	int schaden = 5; };
+	int schaden = 5;
+
+    public:
+        void setName(string parameter1) {
+            name = parameter1;
+        }
+
+        string getName()
+        {
+            return name;
+        }
+
+        void setLeben(int parameter1){
+            leben = parameter1;
+        }
+
+        int getLeben(){
+            return leben;
+        }
+
+        void lebenabZiehen(){
+            leben = leben - schaden;
+        }
+};
